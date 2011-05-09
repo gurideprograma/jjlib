5033d12685ac61b3d5a5b21ecdec608f34e61317
webmaster.rs@gmail.com <webmaster.rs@gmail.com@12f807d6-703e-0410-90b9-d9dbbf6f86db>
2011-05-09 19:34:07 +0000

    Inclusão da função backup() que faz backup do banco de dados e envia para um e-mail ou cria um arquivo .sql no servidor.

--- crislib.php
+++ crislib.php
@@ -4,9 +4,9 @@
 * @name crisLib
 * @author Tiago Floriano <contato@paico.com.br>
 * @author Colaboração direta - Leo Caseiro <www.leocaseiro.com.br> - documentou e melhorou a versão anterior
-* @version 0.15
+* @version 0.17
 * @license http://creativecommons.org/licenses/by-sa/3.0/legalcode
-* @link http://paico.com.br/crislib
+* @link http://paico.com.br/crislib 
 */
 
 /**
@@ -555,6 +555,64 @@ function ison($tabela_de_usuarios, $tabela_de_sessoes = false){
 			return true;
 		}
 	}
+}
+
+/**
+ * Faz backup completo do banco de dados
+ * @name backup
+ * @author David Walsh <davidwalsh.name>
+ * @link http://davidwalsh.name/backup-mysql-database-php
+ * @author Tiago Floriano <contato@paico.com.br> - adaptação do script para uso na crislib de forma mais prática e objetiva
+ * @example backup("meu@email.com"); ou backup(); //informando o e-mail, o backup é enviado por e-mail se não, cria um arquivo .sql
+ * @param string|boolean $sendmail
+ */
+function backup($sendmail = false){
+    #  backup($host,$user,$pass,$name,$tables = '*')
+    #  $link = mysql_connect($host,$user,$pass);
+    #  mysql_select_db($name,$link);
+    $tables == '*';
+    //get all of the tables
+    if($tables == '*'){
+        $tables = array();
+        $result = mysql_query('SHOW TABLES');
+        while($row = mysql_fetch_row($result)){
+            $tables[] = $row[0];
+        }
+    }else{
+        $tables = is_array($tables) ? $tables : explode(',',$tables);
+    }
+
+    //cycle through
+    foreach($tables as $table){
+        $result = mysql_query('SELECT * FROM '.$table);
+        $num_fields = mysql_num_fields($result);
+
+        $return.= 'DROP TABLE '.$table.';';
+        $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+        $return.= "\n\n".$row2[1].";\n\n";
+
+        for ($i = 0; $i < $num_fields; $i++){
+            while($row = mysql_fetch_row($result)){
+                $return.= 'INSERT INTO '.$table.' VALUES(';
+                for($j=0; $j<$num_fields; $j++){
+                    $row[$j] = addslashes($row[$j]);
+                    $row[$j] = ereg_replace("\n","\\n",$row[$j]);
+                    if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
+                    if ($j<($num_fields-1)) { $return.= ','; }
+                }
+                $return.= ");\n";
+            }
+        }
+        $return.="\n\n\n";
+    }
+    if($sendmail == true){
+        mail($sendmail,"[backup]",$return,"From: naoresponda <naoresponda@naoresponda.com>");
+    }else{
+        //save file
+        $handle = fopen('backup_'.date("Ymd-His").'.sql','w+');
+        fwrite($handle,$return);
+        fclose($handle);
+    }
 }
 
 /**
