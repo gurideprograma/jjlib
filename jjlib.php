9698ca49b7d89c447faf69a0e3829da64d7ddfb0
Guri de Programa <gurideprograma@mail.com>
2013-01-08 18:37:45 -0200

    Arquivo jjlib.php

---
+++ jjlib.php
@@ -0,0 +1,34 @@
+<?
+/*
+ *  jjlib - biblioteca de classes e funções PHP
+ *  Copyright (C) 2013 @_gurideprograma
+ *
+ *  Este programa e software livre; voce pode redistribui-lo e/ou
+ *  modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
+ *  publicada pela Free Software Foundation; tanto a versao 2 da
+ *  Licenca como (a seu criterio) qualquer versao mais nova.
+ *
+ *  Este programa e distribuido na expectativa de ser util, mas SEM
+ *  QUALQUER GARANTIA; sem mesmo a garantia implicita de
+ *  COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
+ *  PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
+ *  detalhes.
+ *
+ *  Voce deve ter recebido uma copia da Licenca Publica Geral GNU
+ *  junto com este programa; se nao, escreva para a Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307, USA.
+ *
+ *  Copia da licenca no diretorio licenca/licenca_en.txt
+ *                                licenca/licenca_pt.txt
+ */
+
+
+/**
+ * @name jjlib <http://github.com/gurideprograma/jjlib>
+ * @author Original by github.com/paico (http://github.com/paico/crislib)
+ * @author Modified by LeoCaseiro <http://www.leocaseiro.com/> 2008-11-28 11:47
+ * @author Modified by @_gurideprograma <github.com/gurideprograma> 2013-01-08 18:20
+ * @author Modified by nome <email ou URI> aaaa-mm-dd hh:mm  //para os proximos que modificarem
+ * @license GPLv2
+ * @since 2013-01-08 18:20
